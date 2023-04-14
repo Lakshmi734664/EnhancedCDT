843d8d6f3c7bda47dbb8ebe00c1c5119f64b5da9
BodduPavanKalyan <pavankalyanboddu1999@gmail.com>
2023-04-14 10:18:25 +0530

    commit
    Helper file added and main method changed

--- src/com/acuver/cdt/EnhancedCDTMain.java
+++ src/com/acuver/cdt/EnhancedCDTMain.java
@@ -1,6 +1,62 @@
 package com.acuver.cdt;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
+import com.acuver.cdt.file.CDTFileReader;
+import com.acuver.cdt.file.CDTHelper;
+
 public class EnhancedCDTMain {
-    public static void main(String args[]) {
-        System.out.println("Hello Java");
-    }
+	public static void main(String args[]) throws IOException {
+
+		Properties prop = null;
+		try {
+			File configFile = new File("C:\\Users\\Admin\\git\\EnhancedCDT\\resources\\config.properties");
+			if (!configFile.exists()) {
+				// Display message and exit if config file does not exist
+				JOptionPane.showMessageDialog(null, "Config file does not exist");
+				System.exit(1);
+			}
+			prop = CDTFileReader.readPropertiesFile(configFile.getPath());
+
+			if (prop != null) {
+				System.out.println(CDTHelper.printmsg("Config file exists"));
+			}
+
+			System.out.println("Properties : " + "\n" + prop + "\n");
+			String CDT_COMPARE_REPORT1 = prop.getProperty("CDT_COMPARE_REPORT1");
+			System.out.println("CDT_COMPARE_REPORT1: " + CDT_COMPARE_REPORT1);
+
+			String CDT_COMPARE_REPORT2 = prop.getProperty("CDT_COMPARE_REPORT2");
+			System.out.println("CDT_COMPARE_REPORT2: " + CDT_COMPARE_REPORT2);
+
+			String CDT_EXPORT_XML1 = prop.getProperty("CDT_EXPORT_XML1");
+			System.out.println("CDT_EXPORT_XML1: " + CDT_EXPORT_XML1);
+
+			String CDT_EXPORT_XML2 = prop.getProperty("CDT_EXPORT_XML2");
+			System.out.println("CDT_EXPORT_XML2: " + CDT_EXPORT_XML2);
+
+			String OUTPUT_DIR = prop.getProperty("OUTPUT_DIR");
+			System.out.println("OUTPUT_DIR: " + OUTPUT_DIR);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		CDTFileReader fileReader = new CDTFileReader();
+		File[] filesList = fileReader.readFilesFromDir("D:\\innovation\\cdtcompare\\samples\\20230403154011");
+		for (File file : filesList) {
+			System.out.println("The files in the 20230403154011 folder are " + file.getName());
+		}
+		CDTFileReader fileReader1 = new CDTFileReader();
+		File[] filesList1 = fileReader.readFilesFromDir("D:\\innovation\\cdtcompare\\samples\\20230413162836");
+		for (File file : filesList1) {
+			System.out.println("The files in the 20230413162836 are " + file.getName());
+		}
+
+	}
 }
