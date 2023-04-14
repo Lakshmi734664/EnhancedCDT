d0dfd35fdec29362db6cfefe2a2898d4b37c6f9f
BodduPavanKalyan <pavankalyanboddu1999@gmail.com>
2023-04-14 16:46:53 +0530

    changes are done

--- src/com/acuver/cdt/EnhancedCDTMain.java
+++ src/com/acuver/cdt/EnhancedCDTMain.java
@@ -1,41 +1,32 @@
 package com.acuver.cdt;
 
-import com.acuver.cdt.file.CDTFileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.acuver.cdt.file.CDTFileReader;
+import com.acuver.cdt.file.CDTHelper;
 
 public class EnhancedCDTMain {
-<<<<<<< HEAD
 	public static void main(String args[]) throws IOException {
 
 		Properties prop = null;
 		try {
-			File configFile = new File("enhancedcdt.properties");
-			if (!configFile.exists()) {
+			File enhancedcdtfile = new File("enhancedcdt.properties");
+			CDTHelper helper = new CDTHelper();
+			if (!enhancedcdtfile.exists()) {
 				// Display message and exit if config file does not exist
-				
+				helper.formPropertiesFileHelpMsg();
 				System.exit(1);
 			}
-			prop = CDTFileReader.readPropertiesFile(configFile.getPath());
-
-			if (prop != null) {
-				System.out.println(CDTHelper.printmsg("Config file exists"));
-			}
+			prop = CDTFileReader.readPropertiesFile(enhancedcdtfile.getPath());
+			CDTFileReader fileReader = new CDTFileReader();
 
 			System.out.println("Properties : " + "\n" + prop + "\n");
+
 			String CDT_REPORT_DIR1 = prop.getProperty("CDT_REPORT_DIR1");
 			System.out.println("CDT_REPORT_DIR1: " + CDT_REPORT_DIR1);
 
-			String CDT_REPORT_DIR2 = prop.getProperty("CDT_REPORT_DIR2");
-			System.out.println("CDT_REPORT_DIR2: " + CDT_REPORT_DIR2);
-
-			String CDT_XMLS1 = prop.getProperty("CDT_XMLS1");
-			System.out.println("CDT_XMLS1: " + CDT_XMLS1);
-
-			String CDT_XMLS2 = prop.getProperty("CDT_XMLS2");
-			System.out.println("CDT_XMLS2: " + CDT_XMLS2);
-
-			String OUTPUT_DIR = prop.getProperty("OUTPUT_DIR");
-			System.out.println("OUTPUT_DIR: " + OUTPUT_DIR);
-			CDTFileReader fileReader = new CDTFileReader();
 			if (CDT_REPORT_DIR1 != null && !CDT_REPORT_DIR1.isEmpty()) {
 
 				File[] filesList = fileReader.readFilesFromDir(CDT_REPORT_DIR1);
@@ -43,9 +34,12 @@ public class EnhancedCDTMain {
 					System.out.println("The files in the CDT_REPORT_DIR1 are " + file.getName());
 				}
 			} else {
-				System.out.println("you don't have the directory for CDT_REPORT_DIR1  please add that");
+				helper.printMsg();
+				System.exit(1);
 			}
 
+			String CDT_REPORT_DIR2 = prop.getProperty("CDT_REPORT_DIR2");
+			System.out.println("CDT_REPORT_DIR2: " + CDT_REPORT_DIR2);
 			if (CDT_REPORT_DIR2 != null && !CDT_REPORT_DIR2.isEmpty()) {
 				CDTFileReader fileReader1 = new CDTFileReader();
 				File[] filesList1 = fileReader.readFilesFromDir(CDT_REPORT_DIR2);
@@ -53,9 +47,12 @@ public class EnhancedCDTMain {
 					System.out.println("The files in the CDT_REPORT_DIR2 are " + file.getName());
 				}
 			} else {
-				System.out.println("you don't have the directory for CDT_REPORT_DIR2 please add that");
+				helper.printMsg();
+				System.exit(1);
 			}
 
+			String CDT_XMLS1 = prop.getProperty("CDT_XMLS1");
+			System.out.println("CDT_XMLS1: " + CDT_XMLS1);
 			if (CDT_XMLS1 != null && !CDT_XMLS1.isEmpty()) {
 
 				File[] filesList = fileReader.readFilesFromDir(CDT_XMLS1);
@@ -63,9 +60,12 @@ public class EnhancedCDTMain {
 					System.out.println("The files in the CDT_XMLS1  are " + file.getName());
 				}
 			} else {
-				System.out.println("you don't have the directory for CDT_XMLS1 please add that");
+				helper.printMsg();
+				System.exit(1);
 			}
 
+			String CDT_XMLS2 = prop.getProperty("CDT_XMLS2");
+			System.out.println("CDT_XMLS2: " + CDT_XMLS2);
 			if (CDT_XMLS2 != null && !CDT_XMLS2.isEmpty()) {
 
 				File[] filesList = fileReader.readFilesFromDir(CDT_XMLS2);
@@ -73,16 +73,8 @@ public class EnhancedCDTMain {
 					System.out.println("The files in the CDT_XMLS2  are " + file.getName());
 				}
 			} else {
-				System.out.println("you don't have the directory for CDT_XMLS2 please add that");
-			}
-
-			if (OUTPUT_DIR != null && !OUTPUT_DIR.isEmpty()) {
-				File[] filesList = fileReader.readFilesFromDir(OUTPUT_DIR);
-				for (File file : filesList) {
-					System.out.println("The files in the OUTPUT_DIR are " + file.getName());
-				}
-			} else {
-				System.out.println("you don't have the directory for OUTPUT_DIR please add that");
+				helper.printMsg();
+				System.exit(1);
 			}
 
 		} catch (IOException e) {
@@ -90,25 +82,5 @@ public class EnhancedCDTMain {
 			e.printStackTrace();
 		}
 
-	}   
-        String fileData = "<Order  DocumentType=\"0001\"  EnterpriseCode=\"Matrix\"  >\r\n"
-        		+ "<OrderHoldTypes>\r\n"
-        		+ "<OrderHoldType HoldType=\"FRAUD_HOLD\"/>\r\n"
-        		+ "</OrderHoldTypes>\r\n"
-        		+ "\r\n"
-        		+ "<OrderLines>\r\n"
-        		+ "<OrderLine OrderedQty=\"1\" >\r\n"
-        		+ "<Item ItemID=\"100013\" UnitCost=\"10.0\" UnitOfMeasure=\"EACH\"/>\r\n"
-        		+ "</OrderLine>\r\n"
-        		+ "</OrderLines>\r\n"
-        		+ "<PersonInfoShipTo AddressLine1=\"234 Copley Place\" City=\"Boston\" Country=\"US\" DayPhone=\"\"  EMailID=\"\" FirstName=\"Lakshmi\" LastName=\"A\" MobilePhone=\"\"  State=\"MA\"  ZipCode=\"02116\"/>\r\n"
-        		+ "<PersonInfoBillTo AddressLine1=\"234 Copley Place\" City=\"Boston\" Country=\"US\" DayPhone=\"\"  EMailID=\"\" FirstName=\"Lakshmi\" LastName=\"A\" MobilePhone=\"\"  State=\"MA\"  ZipCode=\"02116\"/>\r\n"
-        		+ "</Order>\r\n"
-        		+ " ";
-        
-        
-        String fileLocation = "D://Reports//CDT//";
-        String fileName = "test";
-        CDTFileWriter writer  = new CDTFileWriter(fileLocation,fileName,fileData);
-}
+	}
 }
--- src/com/acuver/cdt/file/CDTHelper.java
+++ src/com/acuver/cdt/file/CDTHelper.java
@@ -1,16 +1,21 @@
 package com.acuver.cdt.file;
 
-import javax.swing.JOptionPane;
-
 public class CDTHelper {
-        
-        public static String printmsg(String message) {
-            JOptionPane.showMessageDialog(null, message);
-			return message;
-        }
-        
-        public static void formpropertiesfilehelpmsg() {
-            String message = "This is a help message for the form properties file.";
-            JOptionPane.showMessageDialog(null, message, "Form Properties File Help", JOptionPane.INFORMATION_MESSAGE);
-        }
+
+	public void printMsg() {
+		String message = "properties does'nt exist please add that property";
+		System.out.println(message);
+	}
+
+	public void formPropertiesFileHelpMsg() {
+		String message = "Please create a file enhancedcdt.properties in current folder. The following properties can be configured.\r\n"
+				+ "	CDT_REPORT_DIR1     CDT Compare Report1\r\n"
+				+ "	CDT_REPORT_DIR2     CDT Compare Report2\r\n"
+				+ "	CDT_XMLS1  	    CDT Export XMLs1\r\n"
+				+ "	CDT_XMLS2  	    CDT Export XMLs\r\n"
+				+ "	YDKPREF1	    ydfprefs.xml (Optional)\r\n"
+				+ "        YDKPERF2	    ydfprefs.xml (Optional)\r\n"
+				+ "        OUTPUT_DIR 	    Output Folder (Optional)";
+		System.out.println(message);
+	}
 }
