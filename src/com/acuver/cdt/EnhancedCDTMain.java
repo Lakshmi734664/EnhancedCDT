d5286a4df81f583e7f46d6077cf5d1379f35da2c
BodduPavanKalyan <pavankalyanboddu1999@gmail.com>
2023-04-14 14:46:59 +0530

    commit

--- src/com/acuver/cdt/EnhancedCDTMain.java
+++ src/com/acuver/cdt/EnhancedCDTMain.java
@@ -14,10 +14,10 @@ public class EnhancedCDTMain {
 
 		Properties prop = null;
 		try {
-			File configFile = new File("C:\\Users\\Admin\\git\\EnhancedCDT\\resources\\config.properties");
+			File configFile = new File("enhancedcdt.properties");
 			if (!configFile.exists()) {
 				// Display message and exit if config file does not exist
-				JOptionPane.showMessageDialog(null, "Config file does not exist");
+				
 				System.exit(1);
 			}
 			prop = CDTFileReader.readPropertiesFile(configFile.getPath());
@@ -27,73 +27,67 @@ public class EnhancedCDTMain {
 			}
 
 			System.out.println("Properties : " + "\n" + prop + "\n");
-			String CDT_COMPARE_REPORT1 = prop.getProperty("CDT_COMPARE_REPORT1");
-			System.out.println("CDT_COMPARE_REPORT1: " + CDT_COMPARE_REPORT1);
+			String CDT_REPORT_DIR1 = prop.getProperty("CDT_REPORT_DIR1");
+			System.out.println("CDT_REPORT_DIR1: " + CDT_REPORT_DIR1);
 
-			String CDT_COMPARE_REPORT2 = prop.getProperty("CDT_COMPARE_REPORT2");
-			System.out.println("CDT_COMPARE_REPORT2: " + CDT_COMPARE_REPORT2);
+			String CDT_REPORT_DIR2 = prop.getProperty("CDT_REPORT_DIR2");
+			System.out.println("CDT_REPORT_DIR2: " + CDT_REPORT_DIR2);
 
-			String CDT_EXPORT_XML1 = prop.getProperty("CDT_EXPORT_XML1");
-			System.out.println("CDT_EXPORT_XML1: " + CDT_EXPORT_XML1);
+			String CDT_XMLS1 = prop.getProperty("CDT_XMLS1");
+			System.out.println("CDT_XMLS1: " + CDT_XMLS1);
 
-			String CDT_EXPORT_XML2 = prop.getProperty("CDT_EXPORT_XML2");
-			System.out.println("CDT_EXPORT_XML2: " + CDT_EXPORT_XML2);
+			String CDT_XMLS2 = prop.getProperty("CDT_XMLS2");
+			System.out.println("CDT_XMLS2: " + CDT_XMLS2);
 
 			String OUTPUT_DIR = prop.getProperty("OUTPUT_DIR");
 			System.out.println("OUTPUT_DIR: " + OUTPUT_DIR);
 			CDTFileReader fileReader = new CDTFileReader();
-			if (CDT_COMPARE_REPORT1 != null && !CDT_COMPARE_REPORT1.isEmpty()) {
+			if (CDT_REPORT_DIR1 != null && !CDT_REPORT_DIR1.isEmpty()) {
 
-				File[] filesList = fileReader.readFilesFromDir(CDT_COMPARE_REPORT1);
+				File[] filesList = fileReader.readFilesFromDir(CDT_REPORT_DIR1);
 				for (File file : filesList) {
-					System.out.println("The files in the CDT_COMPARE_REPORT1 are " + file.getName());
+					System.out.println("The files in the CDT_REPORT_DIR1 are " + file.getName());
 				}
-			}
-			else {
-				System.out.println("you don't have the directory for CDT_COMPARE_REPORT1  please add that");
+			} else {
+				System.out.println("you don't have the directory for CDT_REPORT_DIR1  please add that");
 			}
 
-			if (CDT_COMPARE_REPORT2 != null && !CDT_COMPARE_REPORT2.isEmpty()) {
+			if (CDT_REPORT_DIR2 != null && !CDT_REPORT_DIR2.isEmpty()) {
 				CDTFileReader fileReader1 = new CDTFileReader();
-				File[] filesList1 = fileReader.readFilesFromDir(CDT_COMPARE_REPORT2);
+				File[] filesList1 = fileReader.readFilesFromDir(CDT_REPORT_DIR2);
 				for (File file : filesList1) {
-					System.out.println("The files in the CDT_COMPARE_REPORT2 are " + file.getName());
+					System.out.println("The files in the CDT_REPORT_DIR2 are " + file.getName());
 				}
+			} else {
+				System.out.println("you don't have the directory for CDT_REPORT_DIR2 please add that");
 			}
-			else {
-				System.out.println("you don't have the directory for CDT_COMPARE_REPORT2 please add that");
-			}
-			
-			if (CDT_EXPORT_XML1 != null && !CDT_EXPORT_XML1.isEmpty()) {
 
-				File[] filesList = fileReader.readFilesFromDir(CDT_EXPORT_XML1);
+			if (CDT_XMLS1 != null && !CDT_XMLS1.isEmpty()) {
+
+				File[] filesList = fileReader.readFilesFromDir(CDT_XMLS1);
 				for (File file : filesList) {
-					System.out.println("The files in the CDT_EXPORT_XML1  are " + file.getName());
+					System.out.println("The files in the CDT_XMLS1  are " + file.getName());
 				}
+			} else {
+				System.out.println("you don't have the directory for CDT_XMLS1 please add that");
 			}
-			else {
-				System.out.println("you don't have the directory for CDT_EXPORT_XML1 please add that");
-			}
-			
-			if (CDT_EXPORT_XML2 != null && !CDT_EXPORT_XML2.isEmpty()) {
 
-				File[] filesList = fileReader.readFilesFromDir(CDT_EXPORT_XML2);
+			if (CDT_XMLS2 != null && !CDT_XMLS2.isEmpty()) {
+
+				File[] filesList = fileReader.readFilesFromDir(CDT_XMLS2);
 				for (File file : filesList) {
-					System.out.println("The files in the CDT_EXPORT_XML2  are " + file.getName());
+					System.out.println("The files in the CDT_XMLS2  are " + file.getName());
 				}
+			} else {
+				System.out.println("you don't have the directory for CDT_XMLS2 please add that");
 			}
-			else {
-				System.out.println("you don't have the directory for CDT_EXPORT_XML1 please add that");
-			}
-
 
 			if (OUTPUT_DIR != null && !OUTPUT_DIR.isEmpty()) {
 				File[] filesList = fileReader.readFilesFromDir(OUTPUT_DIR);
 				for (File file : filesList) {
 					System.out.println("The files in the OUTPUT_DIR are " + file.getName());
 				}
-			}
-			else {
+			} else {
 				System.out.println("you don't have the directory for OUTPUT_DIR please add that");
 			}
 
