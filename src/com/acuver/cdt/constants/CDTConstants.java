45a6180c7ff8cdc048aa3158fe5339b8e03bcb47
BodduPavanKalyan <pavankalyanboddu1999@gmail.com>
2023-04-24 17:07:58 +0530

    changes

--- src/com/acuver/cdt/constants/CDTConstants.java
+++ src/com/acuver/cdt/constants/CDTConstants.java
@@ -1,19 +1,19 @@
 package com.acuver.cdt.constants;
 
 public class CDTConstants {
-	public final static String CDT_REPORT_DIR1 = "CDT_REPORT_DIR1";
-	public final static String CDT_REPORT_DIR2 = "CDT_REPORT_DIR2";
-	public final static String CDT_XMLS1 = "CDT_XMLS1";
-	public final static String CDT_XMLS2 = "CDT_XMLS2";
-	public final static String OUTPUT_DIR = "OUTPUT_DIR";
-	public final static String YDKPREF1 = "YDKPREF1";
-	public final static String YDKPREF2 = "YDKPREF2";
-	public final static String LockID = "Lockid";
-    public final static String dateFormat = "yyyyMMddHHmmss";
-    public final static String INSERT = "Insert";
-    public final static String DELETE = "Delete";
-    public final static String UPDATE = "Update";
-    public final static String OLDVALUES = "OldValues";
-    public final static String ID = "__ID__";
+	public static final String CDT_REPORT_DIR1 = "CDT_REPORT_DIR1";
+	public static final String CDT_REPORT_DIR2 = "CDT_REPORT_DIR2";
+	public static final String CDT_XMLS1 = "CDT_XMLS1";
+	public static final String CDT_XMLS2 = "CDT_XMLS2";
+	public static final String OUTPUT_DIR = "OUTPUT_DIR";
+	public static final String YDKPREF1 = "YDKPREF1";
+	public static final String YDKPREF2 = "YDKPREF2";
+	public static final String LockID = "Lockid";
+    public static final String dateFormat = "yyyyMMddHHmmss";
+    public static final String INSERT = "Insert";
+    public static final String DELETE = "Delete";
+    public static final String UPDATE = "Update";
+    public static final String OLDVALUES = "OldValues";
+    public static final String ID = "__ID__";
     
 }
