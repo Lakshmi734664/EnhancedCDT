81f08c186f97aa945c330980a9e7a72f46eed46c
Athar6-tech <athar.mulla@acuverconsulting.com>
2023-04-24 21:42:10 +0530

    Updated CDTXmlComparator.java
    Updated CDTXmlComparator.java

--- src/com/acuver/cdt/xml/CDTXmlComparator.java
+++ src/com/acuver/cdt/xml/CDTXmlComparator.java
@@ -5,14 +5,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.TreeMap;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -22,6 +18,9 @@ import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.diff.Diff;
 import org.xmlunit.diff.Difference;
 
+import com.acuver.cdt.EnhancedCDTMain;
+import com.acuver.cdt.constants.CDTConstants;
+
 public class CDTXmlComparator {
 
 	Document inputDoc = null;
@@ -30,8 +29,16 @@ public class CDTXmlComparator {
 	Document updateDoc = null;
 	Document processedUpdateDoc = null;
 	Document processedUpdateEnhancedCompareDoc = null;
-	String primaryKeyName;
-	XPath xPath = XPathFactory.newInstance().newXPath();
+
+	String tablePrefix;
+
+	CDTXmlDifferenceEvaluator CDTXmlDifferenceEvaluator = new CDTXmlDifferenceEvaluator();
+
+	TreeMap<Integer, String> uniqueInsertMap = new TreeMap<Integer, String>();
+	TreeMap<Integer, String> duplicateInsertMap = new TreeMap<Integer, String>();
+
+	TreeMap<Integer, Node> uniqueDeleteMap = new TreeMap<Integer, Node>();
+	TreeMap<Integer, Node> duplicateDeleteMap = new TreeMap<Integer, Node>();
 
 	ArrayList<String> subXmlDiffDataList = new ArrayList<String>();
 	TreeMap<Integer, ArrayList<String>> subXmlDiffDataMap = new TreeMap<Integer, ArrayList<String>>();
@@ -42,7 +49,7 @@ public class CDTXmlComparator {
 	public Document cleanCompareReport(File f) throws Exception {
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		// an instance of builder to parse the specified xml file
+		// An instance of builder to parse the specified xml file
 		DocumentBuilder db = null;
 		db = dbf.newDocumentBuilder();
 		Document doc = null;
@@ -52,9 +59,8 @@ public class CDTXmlComparator {
 		String parentNodeName = root.getNodeName();
 		System.out.println("parentNodeName : " + parentNodeName);
 
-		/* Create DOM Parser */
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder parser = factory.newDocumentBuilder(); // DOM Parser
+		// DOM Parser
+		DocumentBuilder parser = EnhancedCDTMain.factory.newDocumentBuilder();
 
 		// Create a new Processed document with Root Element
 		processedInsertDeleteDoc = parser.newDocument();
@@ -62,9 +68,9 @@ public class CDTXmlComparator {
 		processedInsertDeleteDoc.appendChild(parentElement);
 
 		// Getting the Table Prefix Name
-		primaryKeyName = getTablePrefix(parentNodeName) + "Key";
+		tablePrefix = getTablePrefix(parentNodeName);
+		String primaryKeyName = tablePrefix + "Key";
 		System.out.println("primaryKeyName : " + primaryKeyName);
-		CDTXmlDifferenceEvaluator CDTXmlDifferenceEvaluator = new CDTXmlDifferenceEvaluator();
 		CDTXmlDifferenceEvaluator.setPrimaryKeyName(primaryKeyName);
 
 		// Processing Insert/Delete Tags
@@ -108,153 +114,82 @@ public class CDTXmlComparator {
 	// Processing Insert/Delete Tags
 	public Document removeInsertDeleteElementsWithPrimaryIssue(Document doc) throws Exception {
 
-		String expression = "//Insert";
+		String expression = "//" + CDTConstants.INSERT;
 		NodeList nodeList = null;
-		nodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+		nodeList = (NodeList) EnhancedCDTMain.xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
 		System.out.println("Insert nodeList Length()" + nodeList.getLength());
 		for (int itr = 0; itr < nodeList.getLength(); itr++) {
 			Node insertNode = nodeList.item(itr);
-			System.out.println("\nNode Name :" + insertNode.getNodeName());
-			NamedNodeMap attrList = insertNode.getAttributes();
 
-			for (int attrItr = 0; attrItr < attrList.getLength(); attrItr++) {
-				Node attr = attrList.item(attrItr);
-				String InsertAttrName = attr.getNodeName();
-				if (InsertAttrName.endsWith("Name")) {
-					if (attr.getNodeValue().trim().isEmpty())
-						expression = "//Delete";
+			// Getting Deletes from Insert.
+			NodeList deleteNodeList = getDeletesForInsert(doc, insertNode);
 
-					else
-						expression = "//Delete[@" + InsertAttrName + "=\'" + attr.getNodeValue() + "\']";
+			System.out.println("deleteNodeList length " + deleteNodeList.getLength());
+			for (int itr2 = 0; itr2 < deleteNodeList.getLength(); itr2++) {
+				Node deleteNode = deleteNodeList.item(itr2);
+				int insertNodeIndex = itr;
+				int deleteNodeIndex = itr2;
+				String insertNodeName = insertNode.getNodeName();
+				System.out.println("Comparing insertNode with deleteNode : ");
 
-					System.out.println("\nNode expression :" + expression);
-					NodeList deleteNodeList = null;
-					deleteNodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
-					System.out.println("deleteNodeList length " + deleteNodeList.getLength());
-					for (int itr2 = 0; itr2 < deleteNodeList.getLength(); itr2++) {
-						Node deleteNode = deleteNodeList.item(itr2);
-						int insertNodeIndex = itr;
-						System.out.println("Comparing insertNode with deleteNode : ");
+				Diff diff = DiffBuilder.compare(insertNode).withTest(deleteNode).checkForSimilar().ignoreComments()
+						.ignoreWhitespace().ignoreElementContentWhitespace().normalizeWhitespace()
+						.withDifferenceEvaluator(CDTXmlDifferenceEvaluator).build();
 
-						Diff diff = DiffBuilder.compare(insertNode).withTest(deleteNode).checkForSimilar()
-								.ignoreComments().ignoreWhitespace().ignoreElementContentWhitespace()
-								.normalizeWhitespace().withDifferenceEvaluator(new CDTXmlDifferenceEvaluator()).build();
+				if (diff.hasDifferences()) {
+					Iterator<Difference> iter = diff.getDifferences().iterator();
 
-						if (diff.hasDifferences()) {
-							Iterator<Difference> iter = diff.getDifferences().iterator();
-							System.out.println(
-									"Delete Attribute name:" + deleteNode.getAttributes().item(attrItr).getNodeValue());
-							while (iter.hasNext()) {
-								String datadifference = iter.next().toString();
-								if (datadifference != null && !datadifference.contains("xml version")) {
-									UniqueDiffDataList.add(datadifference);
-								}
-							}
-							if (UniqueDiffDataList != null && UniqueDiffDataList.size() > 0) {
-								uniqueDiffDataMap.put(insertNodeIndex, UniqueDiffDataList);
-							}
-							addUniqueElementsToProcessedDoc(insertNode, deleteNode, insertNodeIndex);
-							break;
-						} else {
-							System.out.println(
-									"No Difference In insertNode and deleteNode.Both Insert and Delete Nodes are removed.");
+					while (iter.hasNext()) {
+						String datadifference = iter.next().toString();
+						if (datadifference != null && !datadifference.contains("xml version")) {
+							UniqueDiffDataList.add(datadifference);
 						}
 					}
-				}
-				if (isSubXML(InsertAttrName, attr.getNodeValue())) {
-					System.out.println("name ends With XML : " + InsertAttrName);
-					expression = "//Delete";
-					System.out.println("\nNode expression In Sub XML : " + expression);
-					NodeList deleteNodeList = null;
-					deleteNodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
-					System.out.println("deleteNodeList length In Sub XML : " + deleteNodeList.getLength());
-					for (int itr2 = 0; itr2 < deleteNodeList.getLength(); itr2++) {
-						Node node2 = deleteNodeList.item(itr2);
-						System.out.println("comparing Sub XML : ");
-						Diff diff = null;
-						NamedNodeMap deleteAttrList = node2.getAttributes();
-						for (int delAttrItr = 0; delAttrItr < deleteAttrList.getLength(); delAttrItr++) {
-							Node delAttr = deleteAttrList.item(delAttrItr);
-							String delAtrrName = delAttr.getNodeName();
-							if (delAtrrName.equalsIgnoreCase(InsertAttrName)) {
-								System.out.println("Inside Sub XML : ");
-
-								diff = DiffBuilder.compare(attr.getNodeValue()).withTest(delAttr.getNodeValue())
-										.checkForSimilar().ignoreComments().ignoreWhitespace()
-										.ignoreElementContentWhitespace().normalizeWhitespace()
-										.withDifferenceEvaluator(new CDTXmlDifferenceEvaluator()).build();
-							}
-						}
-						if (diff != null && diff.hasDifferences()) {
-							Iterator<Difference> iter = diff.getDifferences().iterator();
-							while (iter.hasNext()) {
-								System.out.println("*****Difference Details in Sub XML*********************");
-								String subXmldiffData = iter.next().toString();
-								if (subXmldiffData != null) {
-									subXmlDiffDataList.add(subXmldiffData);
-								}
-							}
-							int insertNodeIndex = itr;
-							if (subXmlDiffDataList != null && subXmlDiffDataList.size() > 0) {
-								subXmlDiffDataMap.put(insertNodeIndex, subXmlDiffDataList);
-							}
-							break;
-						} else {
-							System.out.println("No Difference in Sub XML : ");
-						}
+					if (UniqueDiffDataList != null && UniqueDiffDataList.size() > 0) {
+						uniqueDiffDataMap.put(insertNodeIndex, UniqueDiffDataList);
 					}
-
+					uniqueInsertMap.put(insertNodeIndex, insertNodeName);
+					uniqueDeleteMap.put(deleteNodeIndex, deleteNode);
+				} else {
+					duplicateInsertMap.put(insertNodeIndex, insertNodeName);
+					duplicateDeleteMap.put(deleteNodeIndex, deleteNode);
+					System.out.println(
+							"No Difference In insertNode and deleteNode.Both Insert and Delete Nodes are removed.");
+					break;
 				}
 			}
 		}
+		addUniqueElementsToProcessedDoc();
 		return processedInsertDeleteDoc;
 
 	}
 
-	public void addUniqueElementsToProcessedDoc(Node insertNode, Node deleteNode, int insertNodeIndex) {
+	public void addUniqueElementsToProcessedDoc() {
 
-		Node importedInsertNode = processedInsertDeleteDoc.importNode(insertNode, true);
-		processedInsertDeleteDoc.getDocumentElement().appendChild(importedInsertNode);
-
-		Node importedDeleteNode = processedInsertDeleteDoc.importNode(deleteNode, true);
-		processedInsertDeleteDoc.getDocumentElement().appendChild(importedDeleteNode);
-
-		if (subXmlDiffDataMap.size() > 0) {
-			for (Entry<Integer, ArrayList<String>> n : subXmlDiffDataMap.entrySet()) {
-				if (insertNodeIndex == (n.getKey())) {
-					Element enhancedCompareEle = processedInsertDeleteDoc.createElement("EnhancedCompare");
-					Element attributeEle = processedInsertDeleteDoc.createElement("Attribute");
-					Element oldAttrEle = processedInsertDeleteDoc.createElement("Old");
-					Element newAttrEle = processedInsertDeleteDoc.createElement("New");
-					ArrayList<String> valuesList = n.getValue();
-					for (int i = 0; i < valuesList.size(); i++) {
-						String diffValues = valuesList.get(i);
-						System.out.println("diffValues in subXmlDiffDataMap : " + diffValues);
-						int beginIndex = diffValues.indexOf("@") + 1;
-						int endIndex = diffValues.indexOf("to");
-						String attrName = diffValues.substring(beginIndex, endIndex).trim();
-						System.out.println("AttrName in subXmlDiffDataMap :" + attrName);
-						int oldBeginIndex = diffValues.indexOf("value '") + 7;
-						int oldEndIndex = diffValues.indexOf("' but");
-						String oldAttrValue = diffValues.substring(oldBeginIndex, oldEndIndex).trim();
-						System.out.println("oldAttrValue in subXmlDiffDataMap :" + oldAttrValue);
-						int newBeginIndex = diffValues.indexOf("but was '") + 9;
-						int newEndIndex = diffValues.indexOf("- comparing") - 2;
-						String newAttrValue = diffValues.substring(newBeginIndex, newEndIndex).trim();
-						System.out.println("newAttrValue in subXmlDiffDataMap :" + newAttrValue);
-						oldAttrEle.setAttribute(attrName, oldAttrValue);
-						newAttrEle.setAttribute(attrName, newAttrValue);
+		if (uniqueInsertMap.size() > 0) {
+			for (Entry<Integer, String> m : uniqueInsertMap.entrySet()) {
+				Node insertNode = inputDoc.getElementsByTagName(m.getValue()).item(m.getKey());
+				Node importedInsertNode = processedInsertDeleteDoc.importNode(insertNode, true);
+				if (duplicateInsertMap.size() == 0) {
+					processedInsertDeleteDoc.getDocumentElement().appendChild(importedInsertNode);
+				} else {
+					if (!duplicateInsertMap.containsKey(m.getKey())) {
+						processedInsertDeleteDoc.getDocumentElement().appendChild(importedInsertNode);
 					}
-					String attrName = insertNode.getAttributes().item(0).getNodeName();
-					System.out.println("attrName :" + attrName);
-					attributeEle.setAttribute("Name", attrName);
-					attributeEle.appendChild(oldAttrEle);
-					attributeEle.appendChild(newAttrEle);
-					enhancedCompareEle.appendChild(attributeEle);
-					importedInsertNode.appendChild(enhancedCompareEle);
 				}
 			}
 		}
+		if (uniqueDeleteMap.size() > 0) {
+			for (Entry<Integer, Node> n : uniqueDeleteMap.entrySet()) {
+				Node importedDeleteNode = processedInsertDeleteDoc.importNode(n.getValue(), true);
+				if (duplicateDeleteMap.size() == 0) {
+					processedInsertDeleteDoc.getDocumentElement().appendChild(importedDeleteNode);
+				} else if (!duplicateDeleteMap.containsKey(n.getKey())) {
+					processedInsertDeleteDoc.getDocumentElement().appendChild(importedDeleteNode);
+				}
+			}
+		}
+		
 		processedInsertDeleteDoc.normalizeDocument();
 	}
 
@@ -293,12 +228,13 @@ public class CDTXmlComparator {
 			String name = attr.getNodeName();
 			if (name.endsWith("Name")) {
 				if (attr.getNodeValue().trim().isEmpty()) {
-					expression = "//Delete";
+					expression = "//" + CDTConstants.DELETE;
 				} else {
-					expression = "//Delete[@" + name + "=\'" + attr.getNodeValue() + "\']";
+					expression = "//" + CDTConstants.DELETE  + "[@" + name + "=\'" + attr.getNodeValue() + "\']";
 				}
 				System.out.println("\nNode expression :" + expression);
-				deleteNodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+				deleteNodeList = (NodeList) EnhancedCDTMain.xPath.compile(expression).evaluate(doc,
+						XPathConstants.NODESET);
 			}
 		}
 		return deleteNodeList;
