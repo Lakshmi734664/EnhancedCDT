dd1e932061eac6309fe9bee90548604122d274b0
Prakash Singh <prakashsingh9976@gmail.com>
2023-05-18 12:16:45 +0530

    Commit

--- src/com/acuver/cdt/file/CDTFileWriter.java
+++ src/com/acuver/cdt/file/CDTFileWriter.java
@@ -3,6 +3,7 @@ package com.acuver.cdt.file;
 import com.acuver.cdt.EnhancedCDTMain;
 import com.acuver.cdt.util.CDTConstants;
 import com.acuver.cdt.util.CDTHelper;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -26,11 +27,8 @@ import java.util.Date;
 
 public class CDTFileWriter {
 
-	
-
 	public void appendXmlFile(File sourceFile, File destFile) {
 		try {
-
 
 			// Use the factory to create a new DocumentBuilder
 			DocumentBuilder builder = EnhancedCDTMain.factory.newDocumentBuilder();
@@ -64,20 +62,20 @@ public class CDTFileWriter {
 
 	public String createOutDir(String location) throws IOException {
 
-	    String fullPath = "";
+		String fullPath = "";
 		String timeStamp = new SimpleDateFormat(CDTConstants.dateFormat).format(new Date());
 
 		if (location == null) {
 
 			fullPath = CDTConstants.currentDirectory + File.separator + timeStamp;
 
-			createDirectory(fullPath + File.separator+CDTConstants.manual);
+			createDirectory(fullPath + File.separator + CDTConstants.manual);
 			createDirectory(fullPath + File.separator + CDTConstants.enhancedCompare);
 		} else {
 
 			fullPath = location + File.separator + timeStamp;
 
-			createDirectory(fullPath + File.separator+CDTConstants.manual);
+			createDirectory(fullPath + File.separator + CDTConstants.manual);
 			createDirectory(fullPath + File.separator + CDTConstants.enhancedCompare);
 		}
 
@@ -86,80 +84,62 @@ public class CDTFileWriter {
 
 	// Writing File to a Directory
 	public void writeFile(String fullPath, Document outputDoc, String fileName) throws Exception {
-		try {
-			CDTFileWriter fileWriter = new CDTFileWriter();
-			String fileData = CDTHelper.convertDocumentToString(fileWriter.prettyPrintXml(outputDoc));
-			createXMLFile(fullPath, fileName, fileData);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		CDTFileWriter fileWriter = new CDTFileWriter();
+		String fileData = CDTHelper.convertDocumentToString(fileWriter.prettyPrintXml(outputDoc));
+		createXMLFile(fullPath, fileName, fileData);
+
 	}
- 
+
 	public Document prettyPrintXml(Document document) {
-        try {
-        	
+		try {
+
 			EnhancedCDTMain.tf.setAttribute("indent-number", 4); // Adjust the indentation level as needed
-            Transformer transformer = EnhancedCDTMain.tf.newTransformer();
+			Transformer transformer = EnhancedCDTMain.tf.newTransformer();
 			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
 			DOMSource source = new DOMSource(document);
 			StringWriter writer = new StringWriter();
 			StreamResult result = new StreamResult(writer);
-          
+
 			transformer.transform(source, result);
 			return CDTHelper.convertStringToDocument(writer.toString().replaceAll("\\n\\s*\\n", "\n"));
 		} catch (TransformerException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-	public void createXMLFile(String fileLocation, String fileName, String fileData)
-			throws IllegalArgumentException, IOException {
-		FileWriter writer = null;
-		try {
-
-			if (fileData == "") {
-				throw new Exception("No file data given\r\n" + "Please give the filedata.");
-			} else if (fileLocation == "") {
-				throw new Exception("File location is missing.");
-			} else if (fileName == "") {
-				throw new Exception("Filename is missing.");
-			} else {
-				// Create a new file object with the specified file location and name
-				File file = new File(fileLocation + File.separator + fileName);
-
-				// If file doesn't exists, then create it
-				if (!file.exists()) {
-					file.createNewFile();
-				}
-                
-				// Create a new FileWriter object to write to the file
-				writer = new FileWriter(file);
-
-				// Write the file data to the file
-				writer.write(fileData);
-
-			}
-		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			// Close the writer
-			writer.close();
 		}
+		return null;
 	}
 
-	public void createDirectory(String path) {
-
-		try {
-			File directory = new File(path);
-
-			directory.mkdirs();
-		} catch (Exception e) {
-			e.printStackTrace();
+	public void createXMLFile(String fileLocation, String fileName, String fileData) throws IOException {
+		if (fileData == "") {
+			throw new IllegalArgumentException("No file data given\r\n" + "Please give the file data.");
+		} else if (fileLocation == "") {
+			throw new IllegalArgumentException("File location is missing.");
+		} else if (fileName == "") {
+			throw new IllegalArgumentException("Filename is missing.");
 		}
+
+		// Create a new file object with the specified file location and name
+		File file = new File(fileLocation + File.separator + fileName);
+
+		// If the file doesn't exist, create it
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileWriter writer = new FileWriter(file);
+		// Write the file data to the file
+		writer.write(fileData);
+
+		writer.close();
+
+	}
+
+	public void createDirectory(String path) throws SecurityException {
+
+		File directory = new File(path);
+		directory.mkdirs();
 	}
 
 	public void copyFileToDirectory(File sourceFile, String destinationDirectory) throws IOException {
@@ -181,7 +161,7 @@ public class CDTFileWriter {
 
 	public void mergeAfterReview(String parentFolderName) {
 
-		String manualFolderName = parentFolderName  +File.separator+CDTConstants.manual;
+		String manualFolderName = parentFolderName + File.separator + CDTConstants.manual;
 		File manualFolder = new File(manualFolderName);
 		File parentFolder = new File(parentFolderName);
 
@@ -213,89 +193,60 @@ public class CDTFileWriter {
 			}
 		}
 	}
-
 	/*
-	public void readDataFromExcelSheet() {
-		// specify input and output file paths
-		String excelFilePath = "D:\\Table.xlsx";
-		String outputFile = "D:\\EnhancedCDT\\recordIdentifier.config";
-
-		try {
-
-			// create input stream for input file
-			FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
-
-			// create workbook object for input file
-			XSSFWorkbook Workbook = new XSSFWorkbook(inputStream);
-
-			// get the first sheet of the workbook
-			XSSFSheet sheet = Workbook.getSheetAt(0);
-
-			// create FileWriter object for output file
-			FileWriter writer = new FileWriter(outputFile);
-
-			int rows = sheet.getLastRowNum();
-			int cols = sheet.getRow(0).getLastCellNum();
-
-			for (int r = 0; r <= rows; r++) {
-
-				if (r == 0) {
-					continue;
-				}
-
-				XSSFRow row = sheet.getRow(r);
-
-				for (int c = 0; c < cols; c++) {
-
-					if (c == 1) {
-						continue;
-					}
-
-					XSSFCell cell = row.getCell(c);
-
-					if (cell == null) {
-						continue;
-					}
-
-					if (c == 0) {
-						System.out.print(cell.getStringCellValue() + "=");
-						writer.write(cell.getStringCellValue() + "=");
-					} else if (c==2) {
-						System.out.print(cell.getStringCellValue()+"|");
-						writer.write(cell.getStringCellValue()+"|");
-					} else {
-						String input = cell.getStringCellValue();
-
-						// Create a StringBuilder to build the modified string
-						StringBuilder builder = new StringBuilder(input);
-
-						// Iterate through each character in the StringBuilder
-						for (int i = 0; i < builder.length(); i++) {
-							// Check if the character is '+'
-							if (builder.charAt(i) == '+') {
-								// Replace '+' with ','
-								builder.setCharAt(i, ',');
-							}
-						}
-
-						// Convert the StringBuilder back to a string
-						String output = builder.toString();
-						System.out.print(output);
-						writer.write(output);
-					}
-
-				}
-				// move to the next line in the output file
-				writer.write("\n");
-				System.out.println();
-			}
-			// close input stream and writer
-			inputStream.close();
-			writer.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-*/
+	 * public void readDataFromExcelSheet() { // specify input and output file paths
+	 * String excelFilePath = "D:\\Table.xlsx"; String outputFile =
+	 * "D:\\EnhancedCDT\\recordIdentifier.config";
+	 * 
+	 * try {
+	 * 
+	 * // create input stream for input file FileInputStream inputStream = new
+	 * FileInputStream(new File(excelFilePath));
+	 * 
+	 * // create workbook object for input file XSSFWorkbook Workbook = new
+	 * XSSFWorkbook(inputStream);
+	 * 
+	 * // get the first sheet of the workbook XSSFSheet sheet =
+	 * Workbook.getSheetAt(0);
+	 * 
+	 * // create FileWriter object for output file FileWriter writer = new
+	 * FileWriter(outputFile);
+	 * 
+	 * int rows = sheet.getLastRowNum(); int cols =
+	 * sheet.getRow(0).getLastCellNum();
+	 * 
+	 * for (int r = 0; r <= rows; r++) {
+	 * 
+	 * if (r == 0) { continue; }
+	 * 
+	 * XSSFRow row = sheet.getRow(r);
+	 * 
+	 * for (int c = 0; c < cols; c++) {
+	 * 
+	 * if (c == 1) { continue; }
+	 * 
+	 * XSSFCell cell = row.getCell(c);
+	 * 
+	 * if (cell == null) { continue; }
+	 * 
+	 * if (c == 0) { System.out.print(cell.getStringCellValue() + "=");
+	 * writer.write(cell.getStringCellValue() + "="); } else if (c==2) {
+	 * System.out.print(cell.getStringCellValue()+"|");
+	 * writer.write(cell.getStringCellValue()+"|"); } else { String input =
+	 * cell.getStringCellValue();
+	 * 
+	 * // Create a StringBuilder to build the modified string StringBuilder builder
+	 * = new StringBuilder(input);
+	 * 
+	 * // Iterate through each character in the StringBuilder for (int i = 0; i <
+	 * builder.length(); i++) { // Check if the character is '+' if
+	 * (builder.charAt(i) == '+') { // Replace '+' with ',' builder.setCharAt(i,
+	 * ','); } }
+	 * 
+	 * // Convert the StringBuilder back to a string String output =
+	 * builder.toString(); System.out.print(output); writer.write(output); } } //
+	 * move to the next line in the output file writer.write("\n");
+	 * System.out.println(); } // close input stream and writer inputStream.close();
+	 * writer.close(); } catch (Exception e) { e.printStackTrace(); } }
+	 */
 }
