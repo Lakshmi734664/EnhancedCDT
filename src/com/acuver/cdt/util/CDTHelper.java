1944f0e3591f130be1f6ed00fcfe787fac2f1942
Prakash Singh <prakashsingh9976@gmail.com>
2023-05-19 10:43:29 +0530

    commit
    removed unused import statements

--- src/com/acuver/cdt/util/CDTHelper.java
+++ src/com/acuver/cdt/util/CDTHelper.java
@@ -1,20 +1,18 @@
 package com.acuver.cdt.util;
 
-import com.acuver.cdt.EnhancedCDTMain;
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringReader;
-import java.io.StringWriter;
+import com.acuver.cdt.EnhancedCDTMain;
 
 public class CDTHelper {
 
