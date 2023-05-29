488be9ff12a93ba0bc7521e32f8469196e958ca1
Athar6-tech <athar.mulla@acuverconsulting.com>
2023-05-29 15:51:31 +0530

    Updated.
    Updated.

--- src/com/acuver/cdt/EnhancedCDTMain.java
+++ src/com/acuver/cdt/EnhancedCDTMain.java
@@ -18,67 +18,66 @@ import java.util.Map;
 
 public class EnhancedCDTMain {
 
-    public static XPath xPath = XPathFactory.newInstance().newXPath();
-    public static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    public static TransformerFactory tf = TransformerFactory.newInstance();
-    // properties
-    public static String CDT_REPORT_DIR1 = null;
-    public static String CDT_REPORT_DIR2 = null;
-    public static String CDT_XMLS1 = null;
-    public static String CDT_XMLS2 = null;
-    public static String OUTPUT_DIR = null;
-    public static String YDKPREF1 = null;
-    public static String YDKPREF2 = null;
+	public static XPath xPath = XPathFactory.newInstance().newXPath();
+	public static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	public static TransformerFactory tf = TransformerFactory.newInstance();
+	// properties
+	public static String CDT_REPORT_DIR1 = null;
+	public static String CDT_REPORT_DIR2 = null;
+	public static String CDT_XMLS1 = null;
+	public static String CDT_XMLS2 = null;
+	public static String OUTPUT_DIR = null;
+	public static String YDKPREF1 = null;
+	public static String YDKPREF2 = null;
 	public static ArrayList<String> ydkPerfIgnoreTables = null;
 
-    // merged reports directories
-    public static String CDT_REPORT_DIR1_OUT = null;
-    public static String CDT_REPORT_DIR2_OUT = null;
-    public static String CDT_REPORT_OUT_DIR1 = null;
-    public static String CDT_REPORT_OUT_DIR2 = null;
+	// merged reports directories
+	public static String CDT_REPORT_DIR1_OUT = null;
+	public static String CDT_REPORT_DIR2_OUT = null;
+	public static String CDT_REPORT_OUT_DIR1 = null;
+	public static String CDT_REPORT_OUT_DIR2 = null;
 
-    public static Map<String, String> recordIdentifierMap;
+	public static Map<String, String> recordIdentifierMap;
 
-    public static void main(String[] argMode) throws Exception {
-        try {
-            factory.setIgnoringElementContentWhitespace(true);
-            CDTFileReader fileReader = new CDTFileReader();
-            CDTFileWriter fileWriter = new CDTFileWriter();
+	public static void main(String[] argMode) throws Exception {
+		try {
+			factory.setIgnoringElementContentWhitespace(true);
+			CDTFileReader fileReader = new CDTFileReader();
+			CDTFileWriter fileWriter = new CDTFileWriter();
 
-            String mode = argMode.length > 0 ? argMode[0] : "--merge";
+			String mode = argMode.length > 0 ? argMode[0] : "--merge";
 
-            switch (mode) {
-                case "--help":
-                    CDTHelper.showPropertiesFileHelpMsg();
-                    break;
-                case "--merge":
-                    fileReader.readPropertiesFile();
-                    fileReader.populateRecordIdentifier();
-                    mergeCDTReports(fileReader, fileWriter);
-                    //	fileWriter.readDataFromExcelSheet();
-                    break;
-                case "--mergeManualReview":
-                    fileReader.readPropertiesFile();
+			switch (mode) {
+			case "--help":
+				CDTHelper.showPropertiesFileHelpMsg();
+				break;
+			case "--merge":
+				fileReader.readPropertiesFile();
+				fileReader.populateRecordIdentifier();
+				mergeCDTReports(fileReader, fileWriter);
+				// fileWriter.readDataFromExcelSheet();
+				break;
+			case "--mergeManualReview":
+				fileReader.readPropertiesFile();
 				fileWriter.mergeAfterReview(CDT_REPORT_OUT_DIR1);
 				// fileWriter.mergeAfterReview(CDT_REPORT_OUT_DIR2);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private static void mergeCDTReports(CDTFileReader fileReader, CDTFileWriter fileWriter) throws Exception {
-        try {
+	private static void mergeCDTReports(CDTFileReader fileReader, CDTFileWriter fileWriter) throws Exception {
+		try {
 
-            if (YDKPREF1 != null && !YDKPREF1.trim().isEmpty()) {
+			if (YDKPREF1 != null && !YDKPREF1.trim().isEmpty()) {
 				ydkPerfIgnoreTables = fileReader.readYDKPrefs(YDKPREF1);
-            }
+			}
 
-            File[] filesList = fileReader.readFilesFromDir(CDT_REPORT_DIR1);
-            if (filesList != null && filesList.length > 0) {
-                CDT_REPORT_DIR1_OUT = fileWriter.createOutDir(OUTPUT_DIR);
-
-                for (File f : filesList) {
+			File[] filesList = fileReader.readFilesFromDir(CDT_REPORT_DIR1);
+			if (filesList != null && filesList.length > 0) {
+				CDT_REPORT_DIR1_OUT = fileWriter.createOutDir(OUTPUT_DIR);
+				for (File f : filesList) {
 					processFile(f, CDT_REPORT_DIR1_OUT, fileReader, fileWriter, true);
 				}
 
@@ -94,51 +93,54 @@ public class EnhancedCDTMain {
 
 				for (File f : filesList) {
 					processFile(f, CDT_REPORT_DIR2_OUT, fileReader, fileWriter, false);
-                }
+				}
 
-            }
+			}
 
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private static void processFile(File f, String outDir, CDTFileReader fileReader, CDTFileWriter fileWriter,
 			boolean isXml1) throws Exception {
-        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
 
-        if(f.isFile()) {
+		if (f.isFile()) {
 
-            if (f != null && f.length() > 0) {
-                String fileName = f.getName();
+			if (f != null && f.length() > 0) {
+				String fileName = f.getName();
 
-                try {
+				try {
 					if (ydkPerfIgnoreTables == null || !(ydkPerfIgnoreTables.contains(fileName))) {
-                        if (fileName.startsWith("YFS") || fileName.startsWith("PLT")) {
-                            CDTXmlComparator xmlComparator = new CDTXmlComparator();
-                            xmlComparator.setInputDoc(documentBuilder.parse(f));
-                            xmlComparator.setOutDir(outDir);
-                            xmlComparator.setFileReader(fileReader);
-                            xmlComparator.setFileWriter(fileWriter);
-							if (!isXml1) {
-								xmlComparator.setXML1(false);
+						if (fileName.startsWith("YFS") || fileName.startsWith("PLT")) {
+							CDTXmlComparator xmlComparator = new CDTXmlComparator();
+							Document inDoc = documentBuilder.parse(f);
+							if (inDoc.getDocumentElement().hasChildNodes()) {
+								xmlComparator.setInputDoc(inDoc);
+								xmlComparator.setOutDir(outDir);
+								xmlComparator.setFileReader(fileReader);
+								xmlComparator.setFileWriter(fileWriter);
+								if (!isXml1) {
+									xmlComparator.setXML1(false);
+								}
+								Document outputDoc = xmlComparator.merge();
+								fileWriter.writeFile(outDir, outputDoc, fileName);
 							}
-                            Document outputDoc = xmlComparator.merge();
-                            fileWriter.writeFile(outDir, outputDoc, fileName);
-                        } else {
-                            try {
-                                fileWriter.copyFileToDirectory(f, outDir);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+						} else {
+							try {
+								fileWriter.copyFileToDirectory(f, outDir);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 
-        }
-    }
+		}
+	}
 }
