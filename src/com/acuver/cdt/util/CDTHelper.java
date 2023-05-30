3a177f5cbb0cfe472dc0be7863e9a85b52e8a158
Prakash Singh <prakashsingh9976@gmail.com>
2023-05-30 16:52:21 +0530

    revert back and commited
    committed changes in showPropertiesFileHelpMsg

--- src/com/acuver/cdt/util/CDTHelper.java
+++ src/com/acuver/cdt/util/CDTHelper.java
@@ -1,18 +1,16 @@
 package com.acuver.cdt.util;
 
-import java.io.StringWriter;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
+import com.acuver.cdt.EnhancedCDTMain;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.acuver.cdt.EnhancedCDTMain;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 public class CDTHelper {
 
@@ -21,7 +19,6 @@ public class CDTHelper {
 	}
 
 	public static void showPropertiesFileHelpMsg() {
-		
 		String message = "Please create a file enhancedcdt.properties in the current folder. The following properties can be configured.\r\n"
 				+ "	CDT_REPORT_DIR1                 Source Env-1 and Target Env-2 CDT Compare Report Folder\r\n"
 				+ "	CDT_REPORT_DIR2 	        Source Env-2 and Target Env-1 CDT Compare Report Folder\r\n"
@@ -32,34 +29,33 @@ public class CDTHelper {
 				+ "        OUTPUT_DIR 			Output Folder(Optional)\r\n"
 				+ "	CDT_REPORT_OUT_DIR1             Used with mergeManualReview option. Source Env-1 and Target Env-2 Enhanced CDT Compare Report Folder\r\n"
 				+ "	CDT_REPORT_OUT_DIR2             Used with mergeManualReview option. Source Env-2 and Target Env-1 Enhanced CDT Compare Report Folder";
-		
+
 		printMsg(message);
 	}
 
-    // Get Table Primary Key Name
-    public static String getTablePrefix(String tableName) {
+	// Get Table Primary Key Name
+	public static String getTablePrefix(String tableName) {
 		int beginIndex = tableName.indexOf(CDTConstants.hyphen);
-        String name = tableName.substring(beginIndex).toLowerCase();
+		String name = tableName.substring(beginIndex).toLowerCase();
 		name = name.replace(CDTConstants.hyphen, " ");
-        char[] charArray = name.toCharArray();
-        boolean foundSpace = true;
-        for (int i = 0; i < charArray.length; i++) {
-            if (Character.isLetter(charArray[i])) {
-                if (foundSpace) {
-                    charArray[i] = Character.toUpperCase(charArray[i]);
-                    foundSpace = false;
-                }
-            } else {
-                foundSpace = true;
-            }
-        }
-        String tablePrefix = String.valueOf(charArray);
+		char[] charArray = name.toCharArray();
+		boolean foundSpace = true;
+		for (int i = 0; i < charArray.length; i++) {
+			if (Character.isLetter(charArray[i])) {
+				if (foundSpace) {
+					charArray[i] = Character.toUpperCase(charArray[i]);
+					foundSpace = false;
+				}
+			} else {
+				foundSpace = true;
+			}
+		}
+		String tablePrefix = String.valueOf(charArray);
 		tablePrefix = tablePrefix.replaceAll(CDTConstants.spaces, "");
-        return tablePrefix;
-    }
+		return tablePrefix;
+	}
 
-
-    public static Element createChildElement(Element element, String childName) {
+	public static Element createChildElement(Element element, String childName) {
 		Document doc = element.getOwnerDocument();
 		Element childElement = doc.createElement(childName);
 		element.appendChild(childElement);
@@ -83,14 +79,17 @@ public class CDTHelper {
 	public static String convertDocumentToString(Document document) {
 		try {
 
-			EnhancedCDTMain.tf.setAttribute("indent-number", 4); // Adjust the indentation level as needed
 			Transformer transformer = EnhancedCDTMain.tf.newTransformer();
-			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
+			document.setXmlStandalone(true);
 
 			StringWriter stringWriter = new StringWriter();
 			transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
-			return stringWriter.toString();
+
+			return stringWriter.toString().replaceAll("\\n\\s*\\n", "\n");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
